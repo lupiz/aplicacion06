4f9c1270865c1f24ddd16093aaba595f1a561c3d
rt <rt>
2015-04-29 14:51:50 -0500

    dos

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,15 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+ $('#btnvibrar').on('tap',function(){
+ navigator. vibrar(1000);
+});//tap a btnvibrar
+$('#btnbeep').on('tap',function (){
+	navegador.beep(1)
+	$('#izquierda').on ('swipeleft',function(){
+		alert('barrio a la izquierda');
+	});
+});//tap vtnbeep
 }); 
 });
 
